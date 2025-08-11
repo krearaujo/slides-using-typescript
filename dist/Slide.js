0b4e0b8793723f81abca81dd096e24819d5cd33b
krearaujo <karenohanaaraujo@gmail.com>
2025-08-11 20:23:17 -0300

    Slides, showing prev, next and pause

---
+++ dist/Slide.js
@@ -0,0 +1,78 @@
+import Timeout from "./TimeOut.js";
+export default class Slide {
+    container;
+    slides;
+    controls;
+    time;
+    index;
+    slide;
+    timeout;
+    paused;
+    pausedTimeout;
+    constructor(container, slides, controls, time = 5000) {
+        this.container = container;
+        this.slides = slides;
+        this.controls = controls;
+        this.time = time;
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.timeout = null;
+        this.init();
+        this.paused = false;
+        this.pausedTimeout = null;
+    }
+    hide(el) {
+        el.classList.remove('active');
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((el) => this.hide(el));
+        this.slide.classList.add("active");
+        this.auto(this.time);
+    }
+    auto(time) {
+        this.timeout?.clear();
+        this.timeout = new Timeout(() => this.next(), time);
+    }
+    prev() {
+        if (this.paused)
+            return;
+        const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    next() {
+        if (this.paused)
+            return;
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    pause() {
+        this.pausedTimeout = new Timeout(() => {
+            this.timeout?.pause();
+            this.paused = true;
+        }, 300);
+    }
+    continue() {
+        this.pausedTimeout?.clear();
+        if (this.paused) {
+            this.paused = false;
+            this.timeout?.continue();
+        }
+    }
+    addControls() {
+        const prevButton = document.createElement("button");
+        const nextButton = document.createElement("button");
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        this.controls.addEventListener("pointerdown", () => this.pause());
+        this.controls.addEventListener("pointerup", () => this.continue());
+        prevButton.addEventListener("pointerup", () => this.next());
+        nextButton.addEventListener("pointerup", () => this.next());
+    }
+    init() {
+        this.addControls();
+        this.show(this.index);
+    }
+}
+//# sourceMappingURL=Slide.js.map
